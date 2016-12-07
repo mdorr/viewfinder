62ff16b17c42e31b06b3bfa0514d0e5bb6556e5b
mspiew <m.a.spiewack@me.com>
2016-12-07 17:08:51 -0500

    Refactor PageHeader to use class definition

--- frontend/components/page_header/page_header.jsx
+++ frontend/components/page_header/page_header.jsx
@@ -1,32 +1,46 @@
 import React from 'react';
 import { Link } from 'react-router';
 
-const sessionLinks = () => (
-  <header className="home-header group">
-    <img className="logoImg" alt="viewfinder logo" src={ window.logoImg } />
-    <nav className="login-signup">
-      <ul>
-        <li><Link to="/login" activeClassName="current">Log in</Link></li>
-        <li className="green"><Link to="/signup" activeClassName="current">Sign up</Link></li>
-      </ul>
-    </nav>
-  </header>
-);
 
-const personalGreeting = (currentUser, logout) => (
-  <header className="home-header group">
-    <img className="logoImg" alt="viewfinder logo" src={ window.logoImg } />
-    <nav className="login-signup">
-      <ul>
-        <li><Link to="/user">{ currentUser.username }</Link></li>
-        <li><a onClick={logout}>Log Out</a></li>
-      </ul>
-    </nav>
-  </header>
-);
+class PageHeader extends React.Component {
+  constructor(props){
+    super(props);
+  }
 
-const PageHeader = ({ currentUser, logout }) => (
-  currentUser ? personalGreeting(currentUser, logout) : sessionLinks()
-);
+  sessionLinks () {
+    return (
+      <header className="home-header group">
+        <img className="logoImg" alt="viewfinder logo" src={ window.logoImg } />
+        <nav className="login-signup">
+          <ul>
+            <li><Link to="/login" activeClassName="current">Log in</Link></li>
+            <li className="green"><Link to="/signup" activeClassName="current">Sign up</Link></li>
+          </ul>
+        </nav>
+      </header>
+    );
+  }
+
+  personalGreeting (currentUser, logout) {
+    return (
+      <header className="home-header group">
+        <img className="logoImg" alt="viewfinder logo" src={ window.logoImg } />
+        <nav className="login-signup">
+          <ul>
+            <li><Link to="/user">{ currentUser.username }</Link></li>
+            <li><a onClick={logout}>Log Out</a></li>
+          </ul>
+        </nav>
+      </header>
+    );
+  }
+
+  render () {
+    return (
+      this.props.currentUser ? this.personalGreeting(this.props.currentUser, this.props.logout) : this.sessionLinks()
+    );
+  }
+
+}
 
 export default PageHeader;
