dd04909c5edf90953efa27e128ee081c797a146d
mspiew <m.a.spiewack@me.com>
2016-12-08 09:30:32 -0500

    Update user profile page

--- frontend/components/user/user.jsx
+++ frontend/components/user/user.jsx
@@ -4,9 +4,26 @@ import { Link, withRouter } from 'react-router';
 class User extends React.Component {
 	constructor(props) {
 		super(props);
+		this.handleFollow = this.handleFollow.bind(this);
+		this.editProfile = this.editProfile.bind(this);
 	}
 
 	// Remember to include componentWillReceiveProps(newProps) (if required)
+
+	editOrFollowButton() {
+		// Should return
+		return (
+			<button onClick={this.handleFollow}  className="profileButton">Edit/Follow</button>
+		);
+	}
+
+	handleFollow () {
+		console.log("Follow/unfollow button clicked");
+	}
+
+	editProfile () {
+		console.log("edit profile button clicked");
+	}
 
 	render() {
 		return (
@@ -15,6 +32,9 @@ class User extends React.Component {
 					<div className="profilePictureLarge">
 
 					</div>
+				</div>
+				<div className="profileButtons">
+					{ this.editOrFollowButton() }
 				</div>
 				<div className="userInfo">
 					<h2>Username</h2>
