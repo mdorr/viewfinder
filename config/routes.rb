6f9fb5bb94a99a4ef3c6b412055a87ad0b12e7fe
mspiew <m.a.spiewack@me.com>
2016-12-12 16:10:15 -0500

    Add routes for photo display

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: "json" } do
     resources :users, only: [:new, :create, :show, :update]
     resource :session, only: [:new, :create, :destroy]
+    resources :photos, only: [:create, :show]
 
     post 'follows', to: 'follows#create'
     delete 'follows', to: 'follows#destroy'
