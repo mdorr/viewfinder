7eb5c3a2fdd00fb3b06147f7e528e830227a92f7
mspiew <m.a.spiewack@me.com>
2016-12-14 10:01:57 -0500

    Made photo description optional

--- frontend/components/photo/photo.jsx
+++ frontend/components/photo/photo.jsx
@@ -3,22 +3,33 @@ import UserBadge from './../user_badge/user_badge';
 
 const Photo = ({ data }) => {
 
-  if (!data.description) {
-    data.description = "IMG_" + data.id;
-  }
-
-  return (
-  <div key={ data.id } className="feedElement">
+  let photoElement = (
     <div className="photo">
       <img src={ data.image_url }></img>
     </div>
+  );
+
+  let photoInfo = (
     <div className="photoInfo">
       <UserBadge user={ data.user } badgeSize='30' fontSize='14' extraPadding='0' />
     </div>
-    <div className="photoDescription">
-      { data.description }
+  );
+
+  let photoDescription = null;
+  if (data.description) {
+    photoDescription = (
+      <div className="photoDescription">
+        { data.description }
+      </div>
+    );
+  }
+
+  return (
+    <div key={ data.id } className="feedElement">
+      { photoElement }
+      { photoInfo }
+      { photoDescription }
     </div>
-  </div>
   );
 };
 export default Photo;
