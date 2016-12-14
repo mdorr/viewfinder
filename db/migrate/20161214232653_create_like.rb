65658d45bcf048b7e3ae76be148d619069c5abed
mspiew <m.a.spiewack@me.com>
2016-12-14 18:31:01 -0500

    Add likes tables to database schema

---
+++ db/migrate/20161214232653_create_like.rb
@@ -0,0 +1,13 @@
+class CreateLike < ActiveRecord::Migration[5.0]
+  def change
+    create_table :likes do |t|
+      t.integer   :user_id, null: false
+      t.integer   :photo_id, null: false
+
+      t.timestamps
+    end
+    add_index :likes, :user_id
+    add_index :likes, :photo_id
+    add_index :likes, [:user_id, :photo_id], unique: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161212200451) do
+ActiveRecord::Schema.define(version: 20161214232653) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,16 @@ ActiveRecord::Schema.define(version: 20161212200451) do
     t.index ["followed_id", "following_id"], name: "index_follows_on_followed_id_and_following_id", unique: true, using: :btree
     t.index ["followed_id"], name: "index_follows_on_followed_id", using: :btree
     t.index ["following_id"], name: "index_follows_on_following_id", using: :btree
+  end
+
+  create_table "likes", force: :cascade do |t|
+    t.integer  "user_id",    null: false
+    t.integer  "photo_id",   null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["photo_id"], name: "index_likes_on_photo_id", using: :btree
+    t.index ["user_id", "photo_id"], name: "index_likes_on_user_id_and_photo_id", unique: true, using: :btree
+    t.index ["user_id"], name: "index_likes_on_user_id", using: :btree
   end
 
   create_table "photos", force: :cascade do |t|
