696e7b37fb261586a34356255b59c532932a33ae
mspiew <m.a.spiewack@me.com>
2016-12-15 16:08:22 -0500

    Remove outdated iframe filter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   helper_method :current_user, :logged_in?
 
-  before_filter :set_p3p
-
   def logged_in?
     return current_user ? true : false
   end
@@ -20,11 +18,4 @@ class ApplicationController < ActionController::Base
     current_user.reset_session_token if current_user
     session[:session_token] = nil
   end
-
-  private
-  # for IE, Facebook, and iframe sessions
-  def set_p3p
-    headers['P3P'] = 'CP="ALL DSP COR CURa ADMa DEVa OUR IND COM NAV"'
-  end
-
 end
