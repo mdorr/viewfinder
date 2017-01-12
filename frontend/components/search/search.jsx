04267db28a57dd6447e8afa19f8ea3357890fff3
mspiew <m.a.spiewack@me.com>
2017-01-12 15:14:10 -0500

    Add search

--- frontend/components/search/search.jsx
+++ frontend/components/search/search.jsx
@@ -18,8 +18,36 @@ class Search extends React.Component {
     return e => this.setState({ [property]: e.target.value });
   }
 
+  componentWillMount () {
+    this.handleSearch();
+  }
+
+  componentWillUpdate(nextProps, nextState) {
+    if (nextState.category != this.state.category) {
+      this.props.clearSearchResults();
+    }
+
+    if (nextState.search != this.state.search) {
+      this.props.router.push({
+        pathname: "/search", query: { search: nextState.search }
+      });
+      if (nextState.search.length > 2) {
+        this.props.newSearch( nextState.search, nextState.category );
+      }
+    }
+
+  }
+
   handleSearch () {
-    this.props.newSearch( this.state.search, this.state.category );
+    if (this.props.router.location.query.search != this.state.search) {
+      this.props.router.push({
+        pathname: "/search",
+        query: { search: this.state.search }
+      });
+    }
+    if (this.state.search.length > 2) {
+      this.props.newSearch( this.state.search, this.state.category );
+    }
   }
 
   switchCategories () {
@@ -34,7 +62,7 @@ class Search extends React.Component {
     return (
       <div className="searchPage">
         <div className="searchBar">
-          <input className="searchInput" value={ this.state.search } onChange={ this.update('search') } />
+          <input className="searchInput" value={ this.state.search } onChange={ this.update('search') } placeholder="Enter search terms here, p.e. 'Italy'" />
           <button onClick={ this.handleSearch } className="searchButton"><i className="fa fa-search" aria-hidden="true"></i></button>
           <div onClick={ this.switchCategories } className="searchCategories">
             { this.state.category }
